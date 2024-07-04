90bdd66d5a71abf001ad5f725f444fc195cefef3
priyarajesh7696 <rameshpriyait@gmail.com>
2024-07-04 12:31:04 +0530

    render link  added

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosService = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"https://capstone-backend-quick-shope-5.onrender.com/",
     headers:{
         "Content-Type":"application/json",
     }
